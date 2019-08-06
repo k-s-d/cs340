29bc022d4f5a258a1aa6ef027c538c1dcd14b842
k-s-d <49178506+k-s-d@users.noreply.github.com>
2019-08-05 21:07:36 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_davisk8',
-  password        : '5064',
-  database        : 'cs340_davisk8'
-});
-module.exports.pool = pool;
